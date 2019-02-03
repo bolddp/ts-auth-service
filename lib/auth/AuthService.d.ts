c76ebd93a9d978b7b039e12cc798a62279514338
Daniel Persson <bighatsmallfeet@gmail.com>
2019-02-03 01:14:37 +0100

    exposed verifyJwt

--- lib/auth/AuthService.d.ts
+++ lib/auth/AuthService.d.ts
@@ -1,7 +1,9 @@
 import { SignupData } from "../SignupData";
 import { UserSession } from "../user/UserSession";
 import { LoginData } from "../LoginData";
+import { TokenInfo } from "../TokenInfo";
 export interface AuthService {
+    verifyJwt(header: string): Promise<TokenInfo>;
     signup(signupData: SignupData): Promise<void>;
     login(loginData: LoginData): Promise<UserSession>;
     refresh(authHeader: string): Promise<UserSession>;
--- src/auth/AuthService.ts
+++ src/auth/AuthService.ts
@@ -1,8 +1,10 @@
 import { SignupData } from "../SignupData";
 import { UserSession } from "../user/UserSession";
 import { LoginData } from "../LoginData";
+import { TokenInfo } from "../TokenInfo";
 
 export interface AuthService {
+  verifyJwt(header: string): Promise<TokenInfo>;
   signup(signupData: SignupData): Promise<void>;
   login(loginData: LoginData): Promise<UserSession>;
   refresh(authHeader: string): Promise<UserSession>;
