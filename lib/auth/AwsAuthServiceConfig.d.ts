23fb6b99b3584555265b51b25d35db42bdb63747
Daniel Persson <bighatsmallfeet@gmail.com>
2019-02-06 20:09:36 +0100

    removed obsolete system test

--- lib/auth/AwsAuthServiceConfig.d.ts
+++ lib/auth/AwsAuthServiceConfig.d.ts
@@ -2,7 +2,6 @@ import { AwsCognitoPublicKey } from "../AwsCognitoPublicKey";
 export interface AwsAuthServiceConfig {
     userPoolId: string;
     clientId: string;
-    identityPoolId: string;
     region: string;
     publicCognitoKeys: AwsCognitoPublicKey[];
 }
--- src/auth/AwsAuthServiceConfig.ts
+++ src/auth/AwsAuthServiceConfig.ts
@@ -3,7 +3,6 @@ import { AwsCognitoPublicKey } from "../AwsCognitoPublicKey";
 export interface AwsAuthServiceConfig {
   userPoolId: string;
   clientId: string;
-  identityPoolId: string;
   region: string;
   publicCognitoKeys: AwsCognitoPublicKey[];
 }
